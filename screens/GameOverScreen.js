35adbc52a8d187aaa00a78cfa3fb4749db0a220f
rayGreycloud <raygreycloud@gmail.com>
2023-04-07 15:08:53 -0700

    start build out game over

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import { Alert, View, Text, StyleSheet } from 'react-native';
 
-const GameOverScreen = () => {
-  return <div>GameOverScreen</div>;
+import PrimaryButton from '../components/ui/PrimaryButton';
+
+const GameOverScreen = ({ resetGameHandler, userNumber }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Your number was {userNumber}</Text>
+      <PrimaryButton onPress={resetGameHandler}>New Game</PrimaryButton>
+    </View>
+  );
 };
 
 export default GameOverScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 24
+  }
+});
