5ea98bba101eee82faf34cd42614a8fa7dc1993c
rayGreycloud <raygreycloud@gmail.com>
2023-04-08 15:10:03 -0700

    build out game over screen

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,13 +1,28 @@
 import React from 'react';
-import { Alert, View, Text, StyleSheet } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 
 import PrimaryButton from '../components/ui/PrimaryButton';
+import Title from '../components/ui/Title';
 
-const GameOverScreen = ({ resetGameHandler, userNumber }) => {
+import Colors from '../constants/colors';
+
+const GameOverScreen = ({ onStartNewGame, roundsNumber, userNumber }) => {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Your number was {userNumber}</Text>
-      <PrimaryButton onPress={resetGameHandler}>New Game</PrimaryButton>
+      <Title style={styles.title}>Game Over</Title>
+      <View style={styles.imageContainer}>
+        <Image
+          source={require('../assets/images/success.png')}
+          style={styles.image}
+        />
+      </View>
+
+      <Text style={styles.text}>
+        Your phone need<Text style={styles.highlight}> {roundsNumber} </Text>
+        rounds to guess the number
+        <Text style={styles.highlight}> {userNumber}</Text>
+      </Text>
+      <PrimaryButton onPress={onStartNewGame}>Start New Game</PrimaryButton>
     </View>
   );
 };
@@ -17,12 +32,34 @@ export default GameOverScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 24,
     alignItems: 'center',
     justifyContent: 'center'
   },
+  title: {
+    color: 'white'
+  },
+  imageContainer: {
+    width: 300,
+    height: 300,
+    borderRadius: 150,
+    borderWidth: 3,
+    borderColor: Colors.primary800,
+    overflow: 'hidden',
+    margin: 36
+  },
+  image: {
+    width: '100%',
+    height: '100%'
+  },
   text: {
+    fontFamily: 'open-sans',
     fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 24
+    marginBottom: 24,
+    textAlign: 'center'
+  },
+  highlight: {
+    fontFamily: 'open-sans-bold',
+    color: Colors.primary500
   }
 });
